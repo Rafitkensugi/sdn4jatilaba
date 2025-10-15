14d07797be222aaa5f5e11330f5bf962175acec1
pariiee <alghifariee8@gmail.com>
2025-10-16 06:29:56 +0700

    memperbaiki hlaman login agar sup darkmode

--- routes/web.php
+++ routes/web.php
@@ -43,7 +43,6 @@ Route::post('/login', [AuthenticatedSessionController::class, 'store']);
 Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
 
 // Profile
-Route::middleware('auth')->group(function () {
 Route::middleware(['auth'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
@@ -54,6 +53,7 @@ Route::middleware(['auth'])->group(function () {
 Route::get('/fasilitas', [FasilitasController::class, 'index'])->name('fasilitas.index');
 Route::get('/fasilitas/{id}', [FasilitasController::class, 'show'])->name('fasilitas.show');
 
+// PPDB / SPMB
 Route::get('/spmb', [PPDBController::class, 'index'])->name('spmb');
 Route::post('/spmb', [PPDBController::class, 'store'])->name('spmb.store');
 
@@ -73,4 +73,4 @@ Route::get('/program', [ProgramController::class, 'index'])->name('program');
 // Visi Misi
 Route::get('/visi-misi', [VisiMisiController::class, 'index'])->name('visi-misi');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
