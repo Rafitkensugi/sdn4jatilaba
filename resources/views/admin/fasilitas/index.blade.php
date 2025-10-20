ebc88af702c121cf898d88b3e5ad3e23367369af
Anton <muhammadanton340@gmail.com>
2025-10-20 08:56:31 +0700

    test

--- resources/views/admin/fasilitas/index.blade.php
+++ resources/views/admin/fasilitas/index.blade.php
@@ -22,7 +22,7 @@
         </div>
     </div>
 </div>
-
+{{-- testttttttttttttttttttt --}}
     <div class="container mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <!-- Success Alert -->
         @if (session('success'))
